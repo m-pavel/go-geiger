b902e3c61c7072a90e9585fcc50feee714b3015f
paullus <paullus325@gmail.com>
2019-09-18 11:11:47 +0300

    sync

--- mqtt/mqtt.go
+++ mqtt/mqtt.go
@@ -27,7 +27,7 @@ func (ts *RadiationService) PrepareCommandLineParams() {
 func (ts RadiationService) Name() string { return "geiger" }
 
 func (ts *RadiationService) Init(client MQTT.Client, topic, topicc, topica string, debug bool) error {
-	ts.g = geiger.New()
+	ts.g = geiger.New(debug)
 	return ts.g.Init(*ts.pin)
 }
 
