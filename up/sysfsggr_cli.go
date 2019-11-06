ce3f84ba0dc85dfabf716d1b79e1d236d3742e61
pavel m <paullus325@gmail.com>
2019-11-06 13:42:18 +0300

    up

--- up/sysfsggr_cli.go
+++ up/sysfsggr_cli.go
@@ -5,16 +5,12 @@ import (
 
 	"fmt"
 
-	"periph.io/x/periph/conn/gpio"
 	"periph.io/x/periph/host"
 	"periph.io/x/periph/host/sysfs"
 )
 
 func main() {
 	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-	if err := sysfs.Pins[402].In(gpio.PullDown, gpio.BothEdges); err != nil {
 		log.Fatal(err)
 	}
 	for i := 0; i < 50; i++ {
