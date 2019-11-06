3622d6331be6e88f6c94494d2b81b63747586e6c
pavel m <paullus325@gmail.com>
2019-11-06 13:42:02 +0300

    up

--- up/sysfsggr_cli.go
+++ up/sysfsggr_cli.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
+	"fmt"
+
+	"periph.io/x/periph/conn/gpio"
 	"periph.io/x/periph/host"
 	"periph.io/x/periph/host/sysfs"
 )
@@ -12,7 +14,13 @@ func main() {
 	if _, err := host.Init(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(sysfs.Pins[402])
+	if err := sysfs.Pins[402].In(gpio.PullDown, gpio.BothEdges); err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < 50; i++ {
+		fmt.Println(sysfs.Pins[402].Read())
+	}
+
 	//p := sysfs.Pin{number: 42, name: "foo", root: "/tmp/gpio/priv/"}
 	//if l := p.Read(); l != gpio.Low {
 	//	t.Fatal("broken pin is always low")
