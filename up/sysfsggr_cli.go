f859af57b54d45ad9dbd260260e5560234aab28d
pavel m <paullus325@gmail.com>
2019-11-06 13:52:35 +0300

    up

--- up/sysfsggr_cli.go
+++ up/sysfsggr_cli.go
@@ -16,9 +16,13 @@ func main() {
 	if _, err := host.Init(); err != nil {
 		log.Fatal(err)
 	}
-	sysfs.Pins[402].In(gpio.PullNoChange, gpio.RisingEdge)
+	pin := sysfs.Pins[402]
+	if err := pin.In(gpio.PullNoChange, gpio.RisingEdge); err != nil {
+		log.Fatal(err)
+	}
 	for i := 0; i < 50; i++ {
-		res := sysfs.Pins[402].WaitForEdge(time.Minute)
+		fmt.Println(pin.Read())
+		res := pin.WaitForEdge(time.Minute)
 		fmt.Println(res)
 		fmt.Println(sysfs.Pins[402].Read())
 		time.Sleep(500 * time.Millisecond)
